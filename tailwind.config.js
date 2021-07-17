966a583bbe336b13342c6a0aab80367f91a590eb
Frank Stallone III <frank@stallone.dev>
2021-07-17 07:19:38 -0400

    feat: ✨ Using Elena

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      serif: ["ui-serif", "Elena", "Georgia", "serif"],
+    },
     extend: {},
   },
   variants: {

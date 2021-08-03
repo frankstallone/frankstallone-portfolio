d7903de86448846237aef6d2f012e7e64052f45b
Frank Stallone III <frank@stallone.dev>
2021-08-03 06:39:14 -0400

    feat: ✨ Adding logo and branding section to BTC

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import { h } from "preact";
 
 function Footer() {
   return (
-    <footer className="text-center text-amber-amber11">
+    <footer className="flex flex-col text-center text-amber-amber11">
       &copy; {new Date().getFullYear()} Frank Stallone III
       <small className="block text-amber-amber10">
         🚀 Happily built by Astro

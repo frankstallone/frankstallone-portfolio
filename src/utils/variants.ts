c51bea2776570b7ed6cd9c755a878e57a975cc2c
Frank Stallone III <frank@stallone.dev>
2021-08-28 17:45:23 -0400

    refactor: ♻️  Consolidated VARIANT_MAPS into one utility and head meta tags

--- src/utils/variants.ts
+++ src/utils/variants.ts
@@ -1,21 +1,29 @@
 export type Variant = "default" | "btc" | "roll";
 
-export interface VariantColors {
-  textClass: string;
+export interface VariantAttributes {
+  primary: string;
+  link: string;
+  hover: string;
   themeColor: string;
 }
 
-export const VARIANT_MAPS: Record<Variant, VariantColors> = {
+export const VARIANT_MAPS: Record<Variant, VariantAttributes> = {
   default: {
-    textClass: "text-amber-amber11",
+    primary: "text-amber-amber12",
+    link: "text-amber-amber12",
+    hover: "hover:text-red-red11",
     themeColor: "#4e2009",
   },
   btc: {
-    textClass: "text-btc-500",
+    primary: "text-btc-500",
+    link: "text-btc-200",
+    hover: "hover:text-btc-100",
     themeColor: "#2c5c9d",
   },
   roll: {
-    textClass: "text-roll-600",
+    primary: "text-btc-500",
+    link: "text-btc-1000",
+    hover: "hover:text-btc-700",
     themeColor: "#2e66d8",
   },
 };

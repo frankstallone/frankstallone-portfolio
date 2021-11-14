3e62051fd3a6178fbbe66a00e1cb4b16d2dd8ab6
Frank Stallone III <frank@stallone.dev>
2021-11-14 09:40:11 -0500

    refactor: ♻️  Breaking animations into other files, adding underlines to links

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,41 +1,57 @@
-import { animate } from "motion";
+import { animate, stagger, timeline } from "motion";
 
+// Breakpoint for large screens for thresholds on InteractionObserver
 const dynamicThreshold = window.innerWidth >= 1024 ? "0.25" : "0.1";
 
-// Simple aninmation for the initial
-animate(
-  ".intro",
-  { transform: "translateY(0)", opacity: 1 },
-  { duration: 1 },
-  { easing: "ease-in-out" }
-);
+// Grab all the sections we want to animate
+const sections = document.querySelectorAll(".section");
 
-// Creating the InteractionObserver
+// Creating a InteractionObserver interface for animations
 const observer = new IntersectionObserver(
   (entries) => {
-    // Each section gets observed
+    // When threshold is met IntersectionObserverEntry section is acted on
     entries.forEach((entry) => {
-      // Animate each section with  animateSections()
+      // Animate each section with animateSections() when intersecting
       if (entry.isIntersecting === true) animateSections(entry.target);
     });
   },
   { threshold: [dynamicThreshold] }
 );
 
-// Grab all the sections we want to animate
-const sections = document.querySelectorAll(".section");
-
-// Taking all sections and attaching IntersectionObserver to them
+// Giving IntersectionObserver targets to watch
 sections.forEach((section) => {
   observer.observe(section);
 });
 
 // Anitmation function
 function animateSections(section) {
-  animate(
-    section,
-    { transform: "scale(1)", opacity: 1 },
-    { duration: 1 },
-    { easing: "ease-in-out" }
-  );
+  // Grab all the token swatches we want to animate
+  const tokens = section.querySelectorAll(".token-swatch");
+
+  // If we have tokens showing and there are tokens in the section
+  if (window.innerWidth >= 768 && tokens) {
+    const sequence = [
+      [
+        section,
+        { transform: "scale(1)", opacity: 1 },
+        { duration: 1 },
+        { easing: "ease-in-out" },
+      ],
+      [
+        tokens,
+        { transform: ["scale(.75)", "scale(1)"], opacity: [0, 1] },
+        { duration: 1, delay: stagger(0.5) },
+        { easing: "ease-in-out" },
+      ],
+    ];
+
+    timeline(sequence);
+  } else {
+    animate(
+      section,
+      { transform: "scale(1)", opacity: 1 },
+      { duration: 1 },
+      { easing: "ease-in-out" }
+    );
+  }
 }

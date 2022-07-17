3044fbf0d684b288f2622cb307f7604896e34c3f
Frank Stallone III <frank@stallone.dev>
2022-07-17 07:59:11 -0400

    config: 🔧 Updating Astro to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,8 @@
-export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  vite: {
-    plugins: [],
-  },
-  buildOptions: {
-    site: "http://stallone.dev", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true, // Generate sitemap (set to "false" to disable)
-  },
-  devOptions: {
-    // port: 3000,         // The port to run the dev server on.
-    tailwindConfig: "./tailwind.config.js",
-  },
-  // renderers: ["@astrojs/renderer-preact"],
-};
+// https://astro.build/config
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: "http://stallone.dev",
+  integrations: [tailwind()],
+});

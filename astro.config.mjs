0fa16e71ed4bb50d1de72f4186c7bcb1201e1329
Frank Stallone <frank@designzen.co>
2023-12-07 06:37:20 -0500

    docs: 📝 Updating config with comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "http://stallone.dev",
   integrations: [tailwind()],
-  base: "/12",
+  // base 12 for adding the contents of /dist/ to the current sites root /12 folder
+  // base: "/12",
 });
